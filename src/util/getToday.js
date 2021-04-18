55c607ed55432270f5c45a8f0c64896a18812a7a
kyupkyup <rudduqdl@naver.com>
2021-04-18 13:27:27 +0900

    fix : https fixed
    deploy https api

--- src/util/getToday.js
+++ src/util/getToday.js
@@ -2,12 +2,15 @@
 function getToday(lat, log) {
   // data fetch again
   //음식 보내주는 api 주소 : http://3.36.171.252:5000/api/v1/food?x=경도&y=위도
-  return fetch("http://3.36.171.252:5000/api/v1/food?x=" + log + "&y=" + lat, {
-    method: "GET",
-    headers: {
-      "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-    },
-  })
+  return fetch(
+    "https://www.weatherplace.site/api/v1/food?x=" + log + "&y=" + lat,
+    {
+      method: "GET",
+      headers: {
+        "Content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+      },
+    }
+  )
     .then(function (resp) {
       // 응답 형식에 따라 resp.text() 가 될 수도 있다
       return resp.json();
